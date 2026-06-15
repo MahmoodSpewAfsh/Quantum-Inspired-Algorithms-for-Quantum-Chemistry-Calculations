e3fe17864384c404ac5b46e6af961334ed20859f
Hadis Salasi <93732815+HadisSalasi@users.noreply.github.com>
2026-06-15 21:11:36 +0330

    Update Creating QAIA Class.py

--- benchmarks for H2/H2 with dSB/Creating QAIA Class.py
+++ benchmarks for H2/H2 with dSB/Creating QAIA Class.py
@@ -1,1 +1,180 @@
+class QAIA:
+    r"""
+    The base class of QAIA.
 
+    This class contains the basic and common functions of all the algorithms.
+
+    Args:
+        J (Union[numpy.array, csr_matrix]): The coupling matrix with shape :math:`(N x N)`.
+        h (numpy.array): The external field with shape :math:`(N x 1)`.
+        x (numpy.array): The initialized spin value with shape :math:`(N x batch_size)`. Default: ``None``.
+        n_iter (int): The number of iterations. Default: ``1000``.
+        batch_size (int): The number of samples. Default: ``1``.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, J, h=None, x=None, n_iter=1000, batch_size=1):
+        """Construct a QAIA algorithm."""
+        self.J = J
+        if h is not None and len(h.shape) < 2:
+            h = h[:, np.newaxis]
+        self.h = h
+        self.x = x
+        # The number of spins
+        self.N = self.J.shape[0]
+        self.n_iter = n_iter
+        self.batch_size = batch_size
+
+    def initialize(self):
+        """Randomly initialize spin values."""
+        self.x = 0.02 * (np.random.rand(self.N, self.batch_size) - 0.5)
+
+    def calc_cut(self, x=None):
+        r"""
+        Calculate cut value.
+
+        Args:
+            x (numpy.array): The spin value with shape :math:`(N x batch_size)`.
+                If ``None``, the initial spin will be used. Default: ``None``.
+        """
+        if x is None:
+            sign = np.sign(self.x)
+        else:
+            sign = np.sign(x)
+
+        return 0.25 * np.sum(self.J.dot(sign) * sign, axis=0) - 0.25 * self.J.sum()
+
+    def calc_energy(self, x=None):
+        r"""
+        Calculate energy.
+
+        Args:
+            x (numpy.array): The spin value with shape :math:`(N x batch_size)`.
+                If ``None``, the initial spin will be used. Default: ``None``.
+        """
+        if x is None:
+            sign = np.sign(self.x)
+        else:
+            sign = np.sign(x)
+
+        if self.h is None:
+            return -0.5 * np.sum(self.J.dot(sign) * sign, axis=0), sign
+        return -0.5 * np.sum(self.J.dot(sign) * sign, axis=0, keepdims=True) - self.h.T.dot(sign), sign
+
+
+class OverflowException(Exception):
+    r"""
+    Custom exception class for handling overflow errors in numerical calculations.
+
+    Args:
+        message: Exception message string, defaults to "Overflow error".
+    """
+
+    def __init__(self, message="Overflow error"):
+        self.message = message
+        super().__init__(self.message)
+"""Coherent Ising Machine with chaotic feedback control algorithm."""
+# pylint: disable=invalid-name
+
+
+
+class SB(QAIA):
+    r"""
+    The base class of SB.
+
+    This class is the base class for SB. It contains the initialization of
+    spin values and momentum.
+
+    Args:
+        J (Union[numpy.array, csr_matrix]): The coupling matrix with shape :math:`(N x N)`.
+        h (numpy.array): The external field with shape :math:`(N, )`.
+        x (numpy.array): The initialized spin value with shape :math:`(N x batch_size)`. Default: ``None``.
+        n_iter (int): The number of iterations. Default: ``1000``.
+        batch_size (int): The number of sampling. Default: ``1``.
+        dt (float): The step size. Default: ``1``.
+        xi (float): positive constant with the dimension of frequency. Default: ``None``.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        J,
+        h=None,
+        x=None,
+        n_iter=1000,
+        batch_size=1,
+        dt=1,
+        xi=None,
+    ):
+        """Construct SB algorithm."""
+        super().__init__(J, h, x, n_iter, batch_size)
+        self.J = csr_matrix(self.J)
+        # positive detuning frequency
+        self.delta = 1
+        self.dt = dt
+        # pumping amplitude
+        self.p = np.linspace(0, 1, self.n_iter)
+        self.xi = xi
+        if self.xi is None:
+            self.xi = 0.5 * np.sqrt(self.N - 1) / np.sqrt(csr_matrix.power(self.J, 2).sum())
+        self.x = x
+
+        self.initialize()
+
+    def initialize(self):
+        """Initialize spin values and momentum."""
+        if self.x is None:
+            self.x = 0.02 * (np.random.rand(self.N, self.batch_size) - 0.5)
+
+        if self.x.shape[0] != self.N:
+            raise ValueError(f"The size of x {self.x.shape[0]} is not equal to the number of spins {self.N}")
+
+        self.y = 0.02 * (np.random.rand(self.N, self.batch_size) - 0.5)
+class DSB(SB):  # noqa: N801
+    r"""
+    Discrete SB algorithm.
+
+    Reference: `High-performance combinatorial optimization based on classical
+    mechanics <https://www.science.org/doi/10.1126/sciadv.abe7953>`_.
+
+    Args:
+        J (Union[numpy.array, csr_matrix]): The coupling matrix with shape :math:`(N x N)`.
+        h (numpy.array): The external field with shape :math:`(N, )`.
+        x (numpy.array): The initialized spin value with shape :math:`(N x batch_size)`. Default: ``None``.
+        n_iter (int): The number of iterations. Default: ``1000``.
+        batch_size (int): The number of sampling. Default: ``1``.
+        dt (float): The step size. Default: ``1``.
+        xi (float): positive constant with the dimension of frequency. Default: ``None``.
+    """
+
+    # pylint: disable=too-many-arguments
+    def __init__(
+        self,
+        J,
+        h=None,
+        x=None,
+        n_iter=1000,
+        batch_size=1,
+        dt=1,
+        xi=None,
+    ):
+        """Construct DSB algorithm."""
+        super().__init__(J, h, x, n_iter, batch_size, dt, xi)
+        self.initialize()
+
+    # pylint: disable=attribute-defined-outside-init
+    def update(self):
+        """Dynamical evolution based on Modified explicit symplectic Euler method."""
+        for i in range(self.n_iter):
+            if self.h is None:
+                self.y += (-(self.delta - self.p[i]) * self.x + self.xi * self.J.dot(np.sign(self.x))) * self.dt
+            else:
+                self.y += (
+                    -(self.delta - self.p[i]) * self.x + self.xi * (self.J.dot(np.sign(self.x)) + self.h)
+                ) * self.dt
+
+            self.x += self.dt * self.y * self.delta
+
+            cond = np.abs(self.x) > 1
+            self.x = np.where(cond, np.sign(self.x), self.x)
+            self.y = np.where(cond, np.zeros_like(self.y), self.y)
